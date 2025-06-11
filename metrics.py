c495d231e4b54afcca5db5558afc8c7382057600
mojimoon <fwang247-c@my.cityu.edu.hk>
2025-06-11 23:55:18 +0800

    update

--- metrics.py
+++ metrics.py
@@ -4,6 +4,9 @@ rewrite of selection_metrics.py in flavor of Keras
 
 import tensorflow as tf
 import numpy as np
+from keras.models import Model
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Dense
 
 def make_batch(X, Y, batch_size):
     for start in range(0, len(X), batch_size):
@@ -45,6 +48,213 @@ def gini_select(X, y, model, budget, batch_size=128):
     selected_idx = idx[:budget]
     return X[selected_idx], y[selected_idx], idx
 
+def extract_layers(model):
+    layers = []
+    for l in model.layers:
+        if isinstance(l, Conv2D):
+            layers.append(('conv', l.output))
+        elif isinstance(l, Dense):
+            layers.append(('dense', l.output))
+    return layers
+
+class kmnc(object):
+    def __init__(self,train,input,layers,k_bins=1000):
+        '''
+        train:训练集数据
+        input:输入张量
+        layers:输出张量层
+        '''
+        self.train=train
+        self.input=input
+        self.layers=layers
+        self.k_bins=k_bins
+        self.lst=[]
+        self.upper=[]
+        self.lower=[]
+        index_lst=[]
+
+        for index,l in layers:
+            self.lst.append(Model(inputs=input,outputs=l))
+            index_lst.append(index)
+            i=Model(inputs=input,outputs=l)
+            if index=='conv':
+                temp=i.predict(train).reshape(len(train),-1,l.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=i.predict(train).reshape(len(train),l.shape[-1])
+            self.upper.append(np.max(temp,axis=0))
+            self.lower.append(np.min(temp,axis=0))
+        self.upper=np.concatenate(self.upper,axis=0)
+        self.lower=np.concatenate(self.lower,axis=0)
+        self.neuron_num=self.upper.shape[0]
+        self.lst=list(zip(index_lst,self.lst))
+
+
+    def fit(self,test):
+        '''
+        test:测试集数据
+        输出测试集的覆盖率
+        '''
+        self.neuron_activate=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            self.neuron_activate.append(temp.copy())
+        self.neuron_activate=np.concatenate(self.neuron_activate,axis=1)
+        act_num=0
+        for index in range(len(self.upper)):
+            bins=np.linspace(self.lower[index],self.upper[index],self.k_bins)
+            act_num+=len(np.unique(np.digitize(self.neuron_activate[:,index],bins)))
+        return act_num/float(self.k_bins*self.neuron_num)
+
+    def rank_fast(self,test):
+        '''
+        test:测试集数据
+        输出排序情况
+        '''
+        self.neuron_activate=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            self.neuron_activate.append(temp.copy())
+        self.neuron_activate=np.concatenate(self.neuron_activate,axis=1)
+        big_bins=np.zeros((len(test),self.neuron_num,self.k_bins+1))
+        for n_index,neuron_activate in enumerate(self.neuron_activate):
+            for index in range(len(neuron_activate)):
+                bins=np.linspace(self.lower[index],self.upper[index],self.k_bins)
+                temp=np.digitize(neuron_activate[index],bins)
+                big_bins[n_index][index][temp]=1
+
+        big_bins=big_bins.astype('int')
+        subset=[]
+        lst=list(range(len(test)))
+        initial=np.random.choice(range(len(test)))
+        lst.remove(initial)
+        subset.append(initial)
+        max_cover_num=(big_bins[initial]>0).sum()
+        cover_last=big_bins[initial]
+        while True:
+            flag=False
+            for index in lst:
+                temp1=np.bitwise_or(cover_last,big_bins[index])
+                now_cover_num=(temp1>0).sum()
+                if now_cover_num>max_cover_num:
+                    max_cover_num=now_cover_num
+                    max_index=index
+                    max_cover=temp1
+                    flag=True
+            cover_last=max_cover
+            if not flag or len(lst)==1:
+                break
+            lst.remove(max_index)
+            subset.append(max_index)
+            # print(max_cover_num)
+        return subset
+
+def kmnc_select(X, y, model, budget, k_bins=1000):
+    layers = extract_layers(model)
+    kmnc_model = kmnc(train=X, input=model.input, layers=layers, k_bins=k_bins)
+    kmnc_model.fit(X)
+    ranked_indices = kmnc_model.rank_fast(X)
+    selected_indices = ranked_indices[:budget]
+    return X[selected_indices], y[selected_indices], selected_indices
+
+class nac(object):
+    def __init__(self,test,input,layers,t=0):
+        self.train=test
+        self.input=input
+        self.layers=layers
+        self.t=t
+        self.lst=[]
+        self.neuron_activate=[]
+        index_lst=[]
+
+        for index,l in layers:
+            self.lst.append(Model(inputs=input,outputs=l))
+            index_lst.append(index)
+            i=Model(inputs=input,outputs=l)
+            if index=='conv':
+                temp=i.predict(test).reshape(len(test),-1,l.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=i.predict(test).reshape(len(test),l.shape[-1])
+            temp=1/(1+np.exp(-temp))
+            self.neuron_activate.append(temp.copy())
+        self.neuron_num=np.concatenate(self.neuron_activate,axis=1).shape[-1]
+        self.lst=list(zip(index_lst,self.lst))
+
+    def fit(self):
+        neuron_activate=0
+        for neu in self.neuron_activate:
+            neuron_activate+=np.sum(np.sum(neu>self.t,axis=0)>0)
+        return neuron_activate/float(self.neuron_num)
+
+    def rank_fast(self,test):
+        self.neuron_activate=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            temp=1/(1+np.exp(-temp))
+            self.neuron_activate.append(temp.copy())
+        self.neuron_activate=np.concatenate(self.neuron_activate,axis=1)
+        upper=(self.neuron_activate>self.t)
+
+        subset=[]
+        lst=list(range(len(test)))
+        initial=np.random.choice(range(len(test)))
+        lst.remove(initial)
+        subset.append(initial)
+        max_cover_num=np.sum(upper[initial])
+        cover_last_1=upper[initial]
+        while True:
+            flag=False
+            for index in lst:
+                temp1=np.bitwise_or(cover_last_1,upper[index])
+                cover1=np.sum(temp1)
+                if cover1>max_cover_num:
+                    max_cover_num=cover1
+                    max_index=index
+                    flag=True
+                    max_cover1=temp1
+            if not flag or len(lst)==1:
+                break
+            lst.remove(max_index)
+            subset.append(max_index)
+            cover_last_1=max_cover1
+            # print(max_cover_num)
+        return subset
+
+    def rank_2(self,test):
+        self.neuron_activate=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            temp=1/(1+np.exp(-temp))
+            self.neuron_activate.append(temp.copy())
+        self.neuron_activate=np.concatenate(self.neuron_activate,axis=1)
+
+        return np.argsort(np.sum(self.neuron_activate>self.t,axis=1))[::-1]
+
+def nac_select(X, y, model, budget, t=0.5):
+    layers = extract_layers(model)
+    nac_model = nac(test=X, input=model.input, layers=layers, t=t)
+    nac_model.fit()
+    ranked_indices = nac_model.rank_fast(X)
+    selected_indices = ranked_indices[:budget]
+    return X[selected_indices], y[selected_indices], selected_indices
+
 def select(X, y, model, budget, metric, batch_size=128, **kwargs):
     if metric == 'rnd':
         return random_select(X, y, budget)
@@ -52,5 +262,9 @@ def select(X, y, model, budget, metric, batch_size=128, **kwargs):
         return entropy_select(X, y, model, budget, batch_size)
     elif metric == 'gini':
         return gini_select(X, y, model, budget, batch_size)
+    elif metric == 'kmnc':
+        return kmnc_select(X, y, model, budget, k_bins=kwargs.get('k_bins', 1000))
+    elif metric == 'nac':
+        return nac_select(X, y, model, budget, t=kwargs.get('t', 0.5))
     else:
         raise NotImplementedError(f"Metric '{metric}' is not implemented.")
--- mnist_exp.py
+++ mnist_exp.py
@@ -15,34 +15,42 @@ test_dir = 'test/mnist' # test/mnist/{test_set}/{model_name}/{selection_metric}/
 metricList = ['rnd', 'ent', 'gini', 'dat', 'gd', 'kmnc', 'nac', 'lsa', 'dsa', 'nc', 'std', 'pace', 'dr', 'ces', 'mcp', 'est']
 budgets = [50, 100, 150, 200]
 
+def onehot_to_int(y):
+    if y.ndim == 2 and y.shape[1] > 1:
+        return np.argmax(y, axis=1)
+    elif y.ndim == 1:
+        return y
+    else:
+        raise ValueError("Input must be a one-hot encoded array or a single-dimensional array.")
+
 def run_selection(model_name, test_set, testX, testy, metricList, budgets):
     model = mnist.get_model(model_name)
 
     for m in metricList:
         for b in budgets:
-            try:
-                selectedX, selectedy, idx = metrics.select(
-                    testX, testy, model, b, m
-                )
-                score = model.evaluate(selectedX, selectedy, verbose=0)
-                test_out_dir = os.path.join(test_dir, test_set, model_name, m, str(b))
-                if not os.path.exists(test_out_dir):
-                    os.makedirs(test_out_dir)
-                # save idx to X.txt and true values to y.txt
-                np.savetxt(os.path.join(test_out_dir, 'X.txt'), idx)
-                np.savetxt(os.path.join(test_out_dir, 'y.txt'), selectedy)
-                with open(out_csv, 'a') as f:
-                    f.write(f'{model_name},{test_set},{m},{b},{score[0]},{score[1]}\n')
-            except Exception as e:
-                with open('log/mnist.log', 'a') as f:
-                    f.write(f'Error with model {model_name}, test_set {test_set}, metric {m}, budget {b}: {str(e)}\n')
+            # try:
+            selectedX, selectedy, idx = metrics.select(
+                testX, testy, model, b, m
+            )
+            score = model.evaluate(selectedX, selectedy, verbose=0)
+            test_out_dir = os.path.join(test_dir, test_set, model_name, m, str(b))
+            if not os.path.exists(test_out_dir):
+                os.makedirs(test_out_dir)
+            # save idx to X.txt and true values to y.txt
+            np.savetxt(os.path.join(test_out_dir, 'X.txt'), idx, fmt='%d')
+            np.savetxt(os.path.join(test_out_dir, 'y.txt'), onehot_to_int(selectedy).astype(int), fmt='%d')
+            with open(out_csv, 'a') as f:
+                f.write(f'{model_name},{test_set},{m},{b},{score[1]}\n')
+            # except Exception as e:
+            #     with open('log/mnist.log', 'a') as f:
+            #         f.write(f'Error with model {model_name}, test_set {test_set}, metric {m}, budget {b}: {str(e)}\n')
 
 def main():
     if not os.path.exists(out_csv):
         with open(out_csv, 'w') as f:
-            f.write('model,test_set,selection_metric,budget,loss,accuracy\n')
+            f.write('model,test_set,selection_metric,budget,accuracy\n')
 
-    run_selection('lenet1', 'mnist', testX, testy, ['rnd', 'ent', 'gini'], budgets)
+    run_selection('lenet1', 'mnist', testX, testy, ['kmnc', 'nac'], budgets)
 
 if __name__ == '__main__':
     main()
